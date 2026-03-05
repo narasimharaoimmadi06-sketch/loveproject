c7b42cc4496d0976c1a74bab5da5c34fbc9950f8
narasimharaoimmadi06-sketch <narasimharaoimmadi06@gmail.com>
2026-03-05 14:23:53 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,25 +5,16 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 import Home from "@/pages/Home";
-import PrivacyPolicy from "@/pages/privacy-policy";
-import Terms from "@/pages/terms";
-import About from "@/pages/about";
-import Contact from "@/pages/contact";
-import Disclaimer from "@/pages/disclaimer";
 import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/privacy-policy" component={PrivacyPolicy} />
-      <Route path="/terms" component={Terms} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/disclaimer" component={Disclaimer} />
       <Route component={NotFound} />
     </Switch>
   );
+}
 }
 
 function App() {
